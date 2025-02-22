6e332b088e346417db15e9764f421c82170f876d
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 11:49:51 +0100

    project initialization

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/EkkeEncoProsampBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.jakab.ekkeencoprosampbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkkeEncoProsampBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkkeEncoProsampBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/jakab/ekkeencoprosampbackend/EkkeEncoProsampBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.jakab.ekkeencoprosampbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkkeEncoProsampBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
