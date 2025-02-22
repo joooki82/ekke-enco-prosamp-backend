63a265d8ffa8727f24727cd079fca4fcdfda2326
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 12:33:11 +0100

    Standard entity model

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/Standard.java
@@ -0,0 +1,45 @@
+package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "standards", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "identifier")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Standard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id; // Auto-incremented primary key
+
+    @Column(name = "standard_number", length = 255, nullable = false)
+    private String standardNumber;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "standard_type", length = 255)
+    private String standardType;
+
+    @Column(name = "identifier", length = 255, nullable = false, unique = true)
+    private String identifier;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
