9006f07860d0a2389895c91270ca0122ef404cab
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 12:33:01 +0100

    SamplingType entity model

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/SamplingType.java
@@ -0,0 +1,39 @@
+ package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sampling_types", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "code")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SamplingType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "code", length = 10, nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "description", columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
