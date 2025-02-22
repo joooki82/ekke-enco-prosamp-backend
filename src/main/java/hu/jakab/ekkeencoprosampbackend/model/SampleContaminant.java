a4c2c98a2333f47748978296f24307a6e5463914
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 14:47:03 +0100

    SampleContaminant entity class

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/SampleContaminant.java
@@ -0,0 +1,34 @@
+package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sample_contaminants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SampleContaminant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sample_id", nullable = false, foreignKey = @ForeignKey(name = "fk_sample_contaminants_sample"))
+    private Sample sample;
+
+    @ManyToOne
+    @JoinColumn(name = "contaminant_id", nullable = false, foreignKey = @ForeignKey(name = "fk_sample_contaminants_contaminant"))
+    private Contaminant contaminant;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
