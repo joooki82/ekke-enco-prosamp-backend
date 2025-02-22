22fe477dce040927405c4b6e80760aa154ce5195
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 12:33:19 +0100

    User entity model

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/User.java
@@ -0,0 +1,43 @@
+package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "email", length = 255, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "role", length = 255, nullable = false)
+    private String role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
