1c5ce3007a23b42f60fb4f6914c7f46d4d6efcb2
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 15:20:29 +0100

    StandardRepository.java interface repository layer

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/repository/StandardRepository.java
@@ -0,0 +1,10 @@
+package hu.jakab.ekkeencoprosampbackend.repository;
+
+import hu.jakab.ekkeencoprosampbackend.model.Standard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StandardRepository extends JpaRepository<Standard, Long> {
+    Optional<Standard> findByIdentifier(String identifier);
+}
