3636098d06ea19ce898a8871fcf0b76d2d386c1f
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 23:28:12 +0100

    remove instance - no need due to spring

--- src/main/java/hu/jakab/ekkeencoprosampbackend/mapper/AdjustmentMethodMapper.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/mapper/AdjustmentMethodMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AdjustmentMethodMapper {
-    AdjustmentMethodMapper INSTANCE = Mappers.getMapper(AdjustmentMethodMapper.class);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
