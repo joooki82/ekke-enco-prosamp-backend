3c02463a77f6c5fdeb97c7e5bfc57f55449138b4
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 15:20:02 +0100

    SamplingRecordEquipmentRepository.java interface repository layer

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/repository/SamplingRecordEquipmentRepository.java
@@ -0,0 +1,7 @@
+package hu.jakab.ekkeencoprosampbackend.repository;
+
+import hu.jakab.ekkeencoprosampbackend.model.SamplingRecordEquipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplingRecordEquipmentRepository extends JpaRepository<SamplingRecordEquipment, Long> {
+}
