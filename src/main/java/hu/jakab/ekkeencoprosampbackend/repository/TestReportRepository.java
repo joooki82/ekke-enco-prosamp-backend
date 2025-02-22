501a3056381c4b2357b01e7a4c13f22e449f4183
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 15:20:37 +0100

    TestReportRepository.java interface repository layer

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/repository/TestReportRepository.java
@@ -0,0 +1,10 @@
+package hu.jakab.ekkeencoprosampbackend.repository;
+
+import hu.jakab.ekkeencoprosampbackend.model.TestReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TestReportRepository extends JpaRepository<TestReport, Long> {
+    Optional<TestReport> findByReportNumber(String reportNumber);
+}
