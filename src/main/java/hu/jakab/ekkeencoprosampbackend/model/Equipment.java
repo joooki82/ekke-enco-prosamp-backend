b7056fe7f137c36abba130862b6d52034dcfd259
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 12:32:38 +0100

    Equipment entity model

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/Equipment.java
@@ -0,0 +1,54 @@
+package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "equipments", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "identifier")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Equipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id; // Auto-incremented primary key
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "producer", length = 255)
+    private String producer;
+
+    @Column(name = "measuring_range", length = 255)
+    private String measuringRange;
+
+    @Column(name = "resolution", length = 255)
+    private String resolution;
+
+    @Column(name = "accuracy", length = 255)
+    private String accuracy;
+
+    @Column(name = "identifier", length = 255, nullable = false, unique = true)
+    private String identifier;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
