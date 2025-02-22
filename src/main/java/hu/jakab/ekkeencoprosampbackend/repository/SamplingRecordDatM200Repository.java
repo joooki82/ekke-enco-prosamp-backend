620af05898f1d0a989289c7aab2be9f42c545aae
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 15:19:53 +0100

    SamplingRecordDatM200Repository.java interface repository layer

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/repository/SamplingRecordDatM200Repository.java
@@ -0,0 +1,7 @@
+package hu.jakab.ekkeencoprosampbackend.repository;
+
+import hu.jakab.ekkeencoprosampbackend.model.SamplingRecordDatM200;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplingRecordDatM200Repository extends JpaRepository<SamplingRecordDatM200, Long> {
+}
