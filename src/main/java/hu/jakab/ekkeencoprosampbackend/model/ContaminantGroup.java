eb5119aceb3220ab97111cd5563d2daa506d341a
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-02-22 14:45:47 +0100

    ContaminantGroup entity class

---
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/model/ContaminantGroup.java
@@ -0,0 +1,36 @@
+package hu.jakab.ekkeencoprosampbackend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contaminant_groups", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "name")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContaminantGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id; // Auto-incremented primary key
+
+    @Column(name = "name", length = 255, nullable = false, unique = true)
+    private String name;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
