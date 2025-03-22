6ca8023a5577d06e793e3b5e7a839a7c72e4f4c9
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-03-22 17:41:24 +0100

    fixing measurement mapper

--- src/main/java/hu/jakab/ekkeencoprosampbackend/mapper/MeasurementUnitMapper.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/mapper/MeasurementUnitMapper.java
@@ -9,12 +9,13 @@ import hu.jakab.ekkeencoprosampbackend.model.MeasurementUnit;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {EntityMapperHelper.class})
 public interface MeasurementUnitMapper {
 
     @Mapping(target = "id", ignore = true) // ID is auto-generated
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "baseUnit", source = "baseUnitId", qualifiedByName = "mapMeasurementUnit")
     MeasurementUnit toEntity(MeasurementUnitRequestDTO dto);
 
     @Mapping(target = "baseUnit", source = "baseUnit") // Explicit mapping
