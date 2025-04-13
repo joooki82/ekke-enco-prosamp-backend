4ddfce848c218601087a878db0e311a08a53a61e
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-04-13 11:02:11 +0200

    validation of RoleRequestDTO.java

--- src/main/java/hu/jakab/ekkeencoprosampbackend/dto/role/RoleRequestDTO.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/dto/role/RoleRequestDTO.java
@@ -1,8 +1,11 @@
 package hu.jakab.ekkeencoprosampbackend.dto.role;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RoleRequestDTO {
+
+    @NotBlank(message = "A szerepkör neve nem lehet üres.")
     private String roleName;
 }
