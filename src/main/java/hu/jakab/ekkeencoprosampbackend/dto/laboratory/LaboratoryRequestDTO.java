54f4c5516e1634abc2e261a1a563973f7aa1f390
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-04-13 10:53:37 +0200

    validation of LaboratoryRequestDTO.java

--- src/main/java/hu/jakab/ekkeencoprosampbackend/dto/laboratory/LaboratoryRequestDTO.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/dto/laboratory/LaboratoryRequestDTO.java
@@ -2,19 +2,26 @@ package hu.jakab.ekkeencoprosampbackend.dto.laboratory;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class LaboratoryRequestDTO {
-    @NotBlank(message = "Name cannot be empty")
+
+    @NotBlank(message = "A laboratórium neve nem lehet üres.")
+    @Size(max = 255, message = "A név legfeljebb 255 karakter lehet.")
     private String name;
 
+    @Size(max = 255, message = "Az akkreditációs szám legfeljebb 255 karakter lehet.")
     private String accreditation;
 
-    @Email(message = "Invalid email format")
+    @Email(message = "Érvénytelen email formátum.")
     private String contactEmail;
 
     private String phone;
--- src/main/java/hu/jakab/ekkeencoprosampbackend/dto/laboratory/LaboratoryResponseDTO.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/dto/laboratory/LaboratoryResponseDTO.java
@@ -1,8 +1,10 @@
 package hu.jakab.ekkeencoprosampbackend.dto.laboratory;
 
 import hu.jakab.ekkeencoprosampbackend.dto.analyticalLabReport.AnalyticalLabReportListItemDTO;
-import hu.jakab.ekkeencoprosampbackend.model.AnalyticalLabReport;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
