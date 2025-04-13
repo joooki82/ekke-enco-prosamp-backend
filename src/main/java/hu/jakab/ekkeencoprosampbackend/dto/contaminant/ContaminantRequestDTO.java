63c46c6bd44d1edb7b9f37f8a0f38053e377b983
joooki82 <61735967+joooki82@users.noreply.github.com>
2025-04-13 10:50:29 +0200

    validation of ContaminantRequestDTO.java

--- src/main/java/hu/jakab/ekkeencoprosampbackend/dto/contaminant/ContaminantRequestDTO.java
+++ src/main/java/hu/jakab/ekkeencoprosampbackend/dto/contaminant/ContaminantRequestDTO.java
@@ -2,19 +2,25 @@ package hu.jakab.ekkeencoprosampbackend.dto.contaminant;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ContaminantRequestDTO {
-    @NotBlank(message = "Name cannot be empty")
+
+    @NotBlank(message = "A szennyező anyag neve nem lehet üres.")
     private String name;
 
+    @Size(max = 255, message = "A leírás legfeljebb 255 karakter lehet.")
     private String description;
 
-    @NotNull(message = "Contaminant group ID cannot be null")
+    @NotNull(message = "A szennyező anyag csoport azonosítója kötelező.")
     private Long contaminantGroupId;
 
 }
